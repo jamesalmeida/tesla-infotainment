e2dc946997ef642a696e2267a6180c0e969f6e1b
James Almeida <703253+jamesalmeida@users.noreply.github.com>
2024-09-24 21:48:32 -0700

    Fix missing classNames on loading screen.

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -7,11 +7,11 @@ const LoadingScreen = () => {
     <div className="loading-screen">
         <div className="displayInsideBezel">
             <div className="circle-container">
-                <div class="circle"></div>
-                <div class="circle-small"></div>
-                <div class="circle-big"></div>
-                <div class="circle-inner-inner"></div>
-                <div class="circle-inner"></div>
+                <div className="circle"></div>
+                <div className="circle-small"></div>
+                <div className="circle-big"></div>
+                <div className="circle-inner-inner"></div>
+                <div className="circle-inner"></div>
             </div>
         <img src={getImagePath('logo-grey.svg')} alt="Tesla Logo" className="tesla-logo spinning" />
       </div>
