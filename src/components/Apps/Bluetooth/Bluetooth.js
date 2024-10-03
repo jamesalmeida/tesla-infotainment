c53dc605edaced9d725ca55b19aa0059f1dea8cd
James Almeida <jimmyplaysdrums@gmail.com>
2024-10-03 15:13:21 -0700

    Cleanup

--- src/components/Apps/Bluetooth/Bluetooth.js
+++ src/components/Apps/Bluetooth/Bluetooth.js
@@ -5,7 +5,7 @@ export const Bluetooth = () => {
   return (
     <div className="bluetooth">
       <h2>Bluetooth</h2>
-      {/* Add your component-specific content here */}
+      {/* Add component-specific content here */}
     </div>
   );
 };
