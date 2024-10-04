f11c16b44cd5193496b562b387da92206172d6f2
James Almeida <jimmyplaysdrums@gmail.com>
2024-10-03 17:25:38 -0700

    Added info to the Software tab in CarSettings.

--- src/components/Apps/CarSettings/components/Software.js
+++ src/components/Apps/CarSettings/components/Software.js
@@ -1,15 +1,56 @@
 export const Software = () => {
     return (
         <div className="buttons-container">
-            <div className="label">Software</div>
-            <div className="flex-row row-1">
-              <div className='btn'>Button</div>
+            <div className='description'>
+                <h3>Tesla Infotainment Simulator (WIP)</h3>
+                <p>Experience a simulated Tesla infotainment system in your browser!</p>
+                
+                <h3>Features to try out:</h3>
+                <ul>
+                    <li>Realistic 3D model of Tesla Model Y. Try opening the trunk and frunk!</li>
+                    <li>Interactive infotainment system simulation. Play a podcast, adjust the volume, etc.</li>
+                    <li>Put the car in reverse (Click the R) and see what's behind you!</li>
+                    <li>Try searching on the map. Turn on traffic or different map styles.</li>
+                    <li>Check out the keyboard shortcuts for more controls.</li>
+                    <li>Responsive design for various screen sizes. Works best on desktop but you can try it out on your phone in landscape mode too.</li>
+                </ul>
+
+                <h3>Keyboard Shortcuts:</h3>
+                <ul>
+                    <li>Shift + Left Arrow: Toggle left turn signal</li>
+                    <li>Shift + Right Arrow: Toggle right turn signal</li>
+                    <li>Shift + H: Toggle hazard lights</li>
+                    <li>Shift + F: Toggle frunk (front trunk)</li>
+                    <li>Shift + T: Toggle trunk</li>
+                    <li>Shift + M: Focus on Navigate Input</li>
+                    <li>Shift + C: Backup Camera</li>
+                </ul>
+
+                <h4>Gear Selection:</h4>
+                <ul>
+                    <li>P: Park</li>
+                    <li>R: Reverse</li>
+                    <li>N: Neutral</li>
+                    <li>D: Drive</li>
+                </ul>
+
+                <h4>Music Controls:</h4>
+                <ul>
+                    <li>Space: Play/Pause</li>
+                    <li>Shift + &lt;: Previous track</li>
+                    <li>Shift + &gt;: Next track</li>
+                </ul>
+
+                <h3>Contributing:</h3>
+                <p>We welcome contributions! Check out the source code and contribute on <a href="https://github.com/jamesalmeida/tesla-infotainment" target="_blank" rel="noopener noreferrer">GitHub</a>!</p>
+
+                <h3>Thanks:</h3>
+                <ul>
+                    <li>Ameer Studio for sharing the <a href="https://sketchfab.com/3d-models/tesla-2018-model-3-5ef9b845aaf44203b6d04e2c677e444f" target="_blank" rel="noopener noreferrer">3D model of the Tesla Model 3</a></li>
+                    <li>Vasilj Miloevi for the <a href="https://codepen.io/eboye/pen/ANPxVX" target="_blank" rel="noopener noreferrer">loading screen circle animations</a></li>
+                    <li>Patrick Stillhart for sharing the <a href="https://codepen.io/arcs/pen/aGzNKY" target="_blank" rel="noopener noreferrer">OutRun video game on Codepen</a></li>
+                </ul>
             </div>
-            <div className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Sed ut purus eget sapien.</div>
-            <div className='gap-row'></div>
-            <div className="flex-row row-2">
-              <div className='btn'>Button</div>
-            </div>
-          </div>
+        </div>
     )
 }
